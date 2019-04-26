86363e23efdb30e6c2b8265114c128a32e0abc4e
Ahad alanazi <ahadamash@gmail.com>
2019-04-26 19:57:13 +0300

    we did it

--- main.js
+++ main.js
@@ -1,95 +1,179 @@
 var hogwarts = {
   location: 'Scotland',
-  houses: [
-    {
-      name: 'Griffindor',
-      img: 'http://images1.wikia.nocookie.net/__cb20120922143611/harrypotter/images/7/70/G_final.jpg',
-      values: [
-        'courage',
-        'bravery',
-        'nerve',
-        'chivalry'
-      ],
-      mascot: 'lion',
-      colors: [
-        'scarlet',
-        'gold'
-      ],
-      founder: 'Godric Gryffindor',
-      students: [
-        'Harry Potter',
-        'Hermione Granger',
-        'Ron Weasley'
-      ]
-    },
-    {
-      name: 'Hufflepuff',
-      img: 'http://images3.wikia.nocookie.net/__cb20111027164827/harrypotter/images/3/3f/H_final.jpg',
-      values: [
-        'hardwork',
-        'patience',
-        'justice',
-        'loyalty'
-      ],
-      mascot: 'badger',
-      colors: [
-        'canary yellow',
-        'black'
-      ],
-      founder: 'Helga Hufflepuff',
-      students: [
-        'Cedric Diggory',
-        'Susan Bones',
-        'Nymphadora Tonks'
-      ]
-    },
-    {
-      name: 'Ravenclaw',
-      img: 'http://images2.wikia.nocookie.net/__cb20111021043857/harrypotter/images/d/da/R_final.jpg',
-      values: [
-        'intelligence',
-        'creativity',
-        'learning',
-        'wit'
-      ],
-      mascot: 'eagle',
-      colors: [
-        'blue',
-        'bronze'
-      ],
-      founder: 'Rowena Ravenclaw',
-      students: [
-        'Luna Lovegood',
-        'Cho Chang',
-        'Padma Patil'
-      ]
-    },
-    {
-      name: 'Slytherin',
-      img: 'http://images3.wikia.nocookie.net/__cb20111027165214/harrypotter/images/d/da/S_final.jpg',
-      values: [
-        'ambition',
-        'cunning',
-        'leadership',
-        'resourcefulness'
-      ],
-      mascot: 'serpent',
-      colors: [
-        'green',
-        'silver'
-      ],
-      founder: 'Salazar Slytherin',
-      students: [
-        'Draco Malfoy',
-        'Pansy Parkinson',
-        'Tom Riddle'
-      ]
-    }
+  houses: [{
+          name: 'Griffindor',
+          img: 'http://images1.wikia.nocookie.net/__cb20120922143611/harrypotter/images/7/70/G_final.jpg',
+          values: [
+              'courage',
+              'bravery',
+              'nerve',
+              'chivalry'
+          ],
+          mascot: 'lion',
+          colors: [
+              'scarlet',
+              'gold'
+          ],
+          founder: 'Godric Gryffindor',
+          students: [
+              'Harry Potter',
+              'Hermione Granger',
+              'Ron Weasley'
+          ]
+      },
+      {
+          name: 'Hufflepuff',
+          img: 'http://images3.wikia.nocookie.net/__cb20111027164827/harrypotter/images/3/3f/H_final.jpg',
+          values: [
+              'hardwork',
+              'patience',
+              'justice',
+              'loyalty'
+          ],
+          mascot: 'badger',
+          colors: [
+              'canary yellow',
+              'black'
+          ],
+          founder: 'Helga Hufflepuff',
+          students: [
+              'Cedric Diggory',
+              'Susan Bones',
+              'Nymphadora Tonks'
+          ]
+      },
+      {
+          name: 'Ravenclaw',
+          img: 'http://images2.wikia.nocookie.net/__cb20111021043857/harrypotter/images/d/da/R_final.jpg',
+          values: [
+              'intelligence',
+              'creativity',
+              'learning',
+              'wit'
+          ],
+          mascot: 'eagle',
+          colors: [
+              'blue',
+              'bronze'
+          ],
+          founder: 'Rowena Ravenclaw',
+          students: [
+              'Luna Lovegood',
+              'Cho Chang',
+              'Padma Patil'
+          ]
+      },
+      {
+          name: 'Slytherin',
+          img: 'http://images3.wikia.nocookie.net/__cb20111027165214/harrypotter/images/d/da/S_final.jpg',
+          values: [
+              'ambition',
+              'cunning',
+              'leadership',
+              'resourcefulness'
+          ],
+          mascot: 'serpent',
+          colors: [
+              'green',
+              'silver'
+          ],
+          founder: 'Salazar Slytherin',
+          students: [
+              'Draco Malfoy',
+              'Pansy Parkinson',
+              'Tom Riddle'
+          ]
+      }
   ]
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   /* DO YOUR CODE HERE */
+  
+  var houseName = []
+  hogwarts.houses.forEach(function(house) {
+      houseName.push(house.name);
+  })
+  console.log(houseName)
+
+  var houseColor = []
+  hogwarts.houses.forEach(function(house) {
+      houseColor.push(house.colors);
+  })
+  console.log(houseColor)
+
+  var houseValues = []
+  hogwarts.houses.forEach(function(house) {
+      houseValues.push(house.values);
+  })
+  console.log(houseValues)
+
+  var houseMascot = []
+  hogwarts.houses.forEach(function(house) {
+      houseMascot.push(house.mascot);
+  })
+  console.log(houseMascot)
+
+  var houseFounder = []
+  hogwarts.houses.forEach(function(house) {
+      houseFounder.push(house.founder);
+  })
+  console.log(houseFounder)
+
+  var houseStudent = []
+  hogwarts.houses.forEach(function(house) {
+      houseStudent.push(house.students);
+  })
+  console.log(houseStudent)
+
+  var houseImg = []
+  hogwarts.houses.forEach(function(house) {
+      houseImg.push(house.img);
+  })
+  var title = Object.keys(hogwarts.houses[0])
+
+
+
+
+  console.log(houseImg)
+
+  for (var i = 0; i < houseName.length; i++) {
+      var $section = $('<section/>');
+      $('main').append($section);
+      var $houseDiv = $('<div/>');
+     
+      $($section).append($houseDiv);
+      $($houseDiv).append($('<img/>').attr('src', houseImg[i]));
+      $($houseDiv).append($('<h2/>').html(houseName[i]));
+      $($houseDiv).append($('<p/>').html(title[5] + ': ' + houseFounder[i]))
+      $($houseDiv).append($('<p/>').html(title[3] + ': ' + houseMascot[i]))
+      $($houseDiv).append($('<h3/>').html(title[2]));
+      
+      var $ulListValue = $('<ul/>')
+      for (var j = 0; j < houseValues[i].length; j++) {
+        $($ulListValue).append($('<li/>').html(houseValues[i][j]));
+      }
+      $($houseDiv).append($ulListValue);
+
+      var $ulListColor = $('<ul/>')
+      $($houseDiv).append($('<h3/>').html(title[4]));
+      for (var k = 0; k < houseColor[i].length; k++) {
+        $($ulListColor).append($('<li/>').html(houseColor[i][k]));
+      }
+      $($houseDiv).append($ulListColor);
+
+      var $ulListStudent = $('<ul/>')
+      $($houseDiv).append($('<h3/>').html(title[6]));
+      for (var c = 0; c < houseStudent[i].length; c++) {
+        $($ulListStudent).append($('<li/>').html(houseStudent[i][c]));
+      }
+      $($houseDiv).append($ulListStudent);
+  }
+  $('section').css('flex-direction', 'row');
+  $('section').parent().css({position : 'relative'});
+  $('section').css({ 'left': 50, position :'relative'});
+
+
 
   // Guidance:  (Feel free to ignore this).
 
