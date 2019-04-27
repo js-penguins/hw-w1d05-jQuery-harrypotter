b77eb5d2a3262dba1aa24e72379f7da3e7c220d1
haithemalbetairi <haithem.t.albetairi@gmail.com>
2019-04-27 13:38:17 +0300

    Finished the hogwarts page

--- main.js
+++ main.js
@@ -114,4 +114,60 @@ $(document).ready(function(){
 
     For example, a function like createList(title, array).
   */
+  var $main = $('main');
+  
+  // loop on the houses and render each
+  hogwarts.houses.forEach(renderHouse); 
+
+  function renderHouse(house){
+    // setting up html elements in variables
+    // <section> contains all the others inside
+    // other elements include and <img>, <h2>, two <p>s
+    var $houseSection = $('<section>'),
+    $houseImg = $('<img>').attr('src', house.img),
+    $houseName = $('<h2>').text(house.name),
+    $founder = $('<p>').text('founder: ' + house.founder),
+    $mascot = $('<p>').text('mascot: ' + house.mascot);
+
+    // appending the created elements to the <section> in order
+    $houseSection.append($houseImg, $houseName, $founder, $mascot);
+
+    // getting all the keys of the house i.e. [ name, img, values,...]
+    // going to use only the [values, colors, students]
+    // all of them are in even positions of the array starting from 2
+    var houseKeys = Object.keys(house);
+
+    for(var i = 2; i <  houseKeys.length; i+=2) {
+      // calling create list which givin a title and array appends them to the
+      // last parameter which here is the $houseSection
+      createList(capitalizeFirst(houseKeys[i]), house[houseKeys[i]], $houseSection)
+    }
+    
+    // append the finished product to the <main> element
+    $main.append($houseSection);
+  }
+
+  // takes a title, array, and section appends them to the
+  // section after creating appropriate elements
+  function createList(title, array, $section) {
+    var $title = $('<h3>').text(title);
+    var $list = $('<ul>')
+
+    array.forEach(function(element) {
+      $list.append($('<li>').text(element));
+    })
+
+    $section.append($title, $list);
+  }
+
+
+  // this is used to capitalize the first letter of the 
+  // keys when creating the list of values, colors and students.
+  function capitalizeFirst(string) {
+    var firstLtr = string.charAt(0).toUpperCase();
+    var restOfString = string.slice(1);
+
+    return firstLtr + restOfString;
+
+  }
 })
