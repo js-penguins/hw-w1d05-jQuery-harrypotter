362b96637381ae20af4ce062ca53d3a633e6606a
Hoda Altalohi <hooda12347@hotmail.com>
2019-04-27 15:49:08 +0300

    finished homework

--- main.js
+++ main.js
@@ -88,6 +88,8 @@ var hogwarts = {
   ]
 }
 
+  
+
 $(document).ready(function(){
   /* DO YOUR CODE HERE */
 
@@ -114,4 +116,88 @@ $(document).ready(function(){
 
     For example, a function like createList(title, array).
   */
-})
+ 
+               // Create element with HTML 
+
+          var h2 = $('<h2>');
+          var p  = $('<p>');
+          var ul = $('<ul>');
+          var li = ul.addClass('<li>');
+          
+       for ( var i=0 ; i < hogwarts.houses.length ; i ++)
+        
+      {
+       var s = $('<section>');
+
+        function mg(){
+          var imgu= $('<img src="'+(hogwarts.houses[i].img)+'"/>');
+         // s.append('<img src="'+imgu+'"/>');
+         s.append(imgu)  
+        }mg();
+          
+        function hn(){
+          h2.text(hogwarts.houses[i].name.toUpperCase());
+          s.append(h2.text().bold()+"<br />");
+          h2.text(" ");
+          s.append(h2.text()+"<br />");
+        }hn();
+
+        function hf(){
+          p.text(hogwarts.houses[i].founder);
+          s.append("founder : "+p.text()+"<br />");  
+        }hf();
+
+        function hm(){
+          p.text(hogwarts.houses[i].mascot);
+          s.append("mascot : "+p.text()+"<br />");
+          p.text(" ");
+          s.append(p.text()+"<br />");
+        }hm();
+        
+        function values (){
+          ul.text("values");
+          s.append(ul.text().bold()+"<br />");
+            
+          for ( var j =0 ;  j <hogwarts.houses[i].values.length ; j++){
+           
+            li.text(hogwarts.houses[i].values[j++]);
+            ul.append(li);
+            s.append(ul.text()+"<br />");
+            $('main').append(s);
+          }
+        }values();  
+        
+        function colors (){
+            ul.text("    ");
+            s.append(ul.text()+"<br />");
+            ul.text("colors");
+            s.append(ul.text().bold()+"<br />");
+            for ( var k =0 ;  k<hogwarts.houses[i].colors.length ; k++){
+              
+              li.text(hogwarts.houses[i].colors[k]);
+              ul.append(li);
+              s.append(ul.text()+"<br />");
+              $('main').append(s);
+            }   
+          }colors();
+          
+        function students (){
+            ul.text("    ");
+            s.append(ul.text()+"<br />");
+            ul.text("students");
+            s.append(ul.text().bold()+"<br />");
+              for ( var f =0 ;  f <hogwarts.houses[i].students.length ; f++){
+                
+                li.text(hogwarts.houses[i].students[f]);
+                ul.append(li);
+                s.append(ul.text()+"<br />");
+                $('main').append(s);
+              }        
+          }students();
+         } 
+         });
+/*
+ div1.text(“your text”)
+ اي بس بدل div1= تسوين div1.text(thing you want in the div)
+ div1.text(students[i]) div1.text[values[i]
+ */
